5d4f1ac87064a27651d01c92c9f4cf9e3e4e7b45
MKim <mrn.kim07@gmail.com>
2023-05-23 15:59:12 -0700

    finish part 3 - fix left arrow

--- src/Card.test.js
+++ src/Card.test.js
@@ -5,24 +5,14 @@ import Card from "./Card";
 //smoke test
 it("renders without crashing", function () {
   render(
-    <Card
-      caption="Photo by Richard Pasquarella on Unsplash"
-      src="image1"
-      currNum="1"
-      totalNum="3"
-    />
+    <Card caption="testing image 1" src="test1.com" currNum="1" totalNum="3" />
   );
 });
 
 //snapshot test
 it("matches snapshot", function () {
   const { container } = render(
-    <Card
-      caption="Photo by Richard Pasquarella on Unsplash"
-      src="image1"
-      currNum="1"
-      totalNum="3"
-    />
+    <Card caption="testing image 1" src="test1.com" currNum="1" totalNum="3" />
   );
   expect(container).toMatchSnapshot();
 });
--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,12 +2,9 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
-it("works when you click on the right arrow", function() {
+it("works when you click on the right arrow", function () {
   const { container } = render(
-    <Carousel
-      photos={TEST_IMAGES}
-      title="images for testing"
-    />
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
   );
   // expect the first image to show, but not the second
   expect(
@@ -30,3 +27,37 @@ it("works when you click on the right arrow", function() {
   ).toBeInTheDocument();
 });
 
+//smoke test
+it("renders without crashing", function () {
+  render(<Carousel photos={TEST_IMAGES} title="images for testing" />);
+});
+
+//snapshot test
+it("matches snapshot", function () {
+  const { container } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it("works when you click on the left arrow", function () {
+  const { container } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+
+  //move to the second image
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  //move back to the first image
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  //expect the first image to show
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).not.toBeInTheDocument();
+});
